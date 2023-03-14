a2535f5af0fb5d5a65712bd2ffe0bd8b5a52edf0
SENACRJEDU\36124872021.1N <fat.jonascampos@gmail.com>
2023-03-14 20:44:51 -0300

    Primeiro commit Aula progWeb2

---
+++ src/main/java/br/com/senac/ExemploRest2Application.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploRest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploRest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/ExemploRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploRest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
