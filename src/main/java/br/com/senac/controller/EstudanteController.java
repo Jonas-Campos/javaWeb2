1605170a1acbc204a4ba9053240ad59fa903c7d1
SENACRJEDU\36124872021.1N <fat.jonascampos@gmail.com>
2023-03-14 21:27:57 -0300

    Ajuste do Controlle

--- src/main/java/br/com/senac/controller/EstudanteController.java
+++ src/main/java/br/com/senac/controller/EstudanteController.java
@@ -3,8 +3,11 @@ package br.com.senac.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +28,23 @@ public class EstudanteController {
 	@GetMapping
 	public ResponseEntity<List<Estudante>> buscarTodosEstudantes(){
 		return estudanteService.buscarTodosEstudantes();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> cadastrarEstudante(@RequestBody Estudante estudante){
+		return estudanteService.cadastrarEstudante(estudante);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> atualizarEstudante
+	(@RequestBody Estudante estudante){
+		return estudanteService.atualizarEstudante(estudante);
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> removerEstudante(@PathVariable Long id){
+		return estudanteService.removerUsuario(id);
 	}
 
 }
