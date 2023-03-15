a4ae1749745feb4ace3d6acd2a9d5ff4fe6722c8
SENACRJEDU\36124872021.1N <fat.jonascampos@gmail.com>
2023-03-14 21:10:07 -0300

    segundo commit

--- src/main/java/br/com/senac/controller/EstudanteController.java
+++ src/main/java/br/com/senac/controller/EstudanteController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,17 @@ public class EstudanteController {
 	@GetMapping
 	public ResponseEntity<List<Estudante>> buscarTodosEstudantes(){
 		return estudanteService.buscarTodosEstudantes();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> cadastrarEstudante(@RequestBody Estudante estudante){
+		return estudanteService.cadastrarEstudante(estudante);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> atualizarEstudante
+	(@RequestBody Estudante estudante){
+		return estudanteService.atualizarEstudante(estudante);
 	}
 
 }
